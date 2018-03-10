bac2169a98ed5a0049b5b5eb7c83f890c1383c46
Unknown <benybudi.bb@gmail.com>
2018-03-10 17:36:03 +0700

    Improved API

--- api.py
+++ api.py
@@ -4,24 +4,32 @@ import RPi.GPIO as GPIO
 app = Flask(__name__)
 
 outputPin = [17, 18, 14, 4]
-inputPin = [20,21]
+inputPin = [17, 18, 14, 4]
 
 
 @app.route("/")
 def turnAllLightOn():
 	return render_template('index.html')
 
+@app.route("toggle/<pin>")
+def toggle(pin):
+	if GPIO.input(outputPin[pin]) == GPIO.HIGH:
+		GPIO.output(outputPin[pin], GPIO.LOW)
+	else 
+		GPIO.output(outputPin[pin], GPIO.HIGH)
+	return 'ok'
+
 @app.route("/turnOnAll")
 def turnAllLightOn():
 	for x in range(len(outputPin)):
 		GPIO.output(outputPin[x], GPIO.HIGH)
-	return render_template('index.html')
+	return 'ok'
 
 @app.route("/turnOffAll")
 def turnAllLightOff():
 	for x in range(len(outputPin)):
 		GPIO.output(outputPin[x], GPIO.LOW)
-	return render_template('index.html')
+	return 'ok'
 
 @app.route("/getdata")
 def getdata():
@@ -37,7 +45,6 @@ GPIO.setmode(GPIO.BCM)
 
 for x in range(len(outputPin)):
 	GPIO.setup(outputPin[x], GPIO.OUT)
-	GPIO.setup(outputPin[x], GPIO.IN)
 
 if __name__ == '__main__':
 	app.run(debug=True)
